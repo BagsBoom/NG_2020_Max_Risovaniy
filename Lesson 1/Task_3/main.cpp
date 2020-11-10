a0456b10824cf3333bb36e47bb6cd2cec59f1d72
Max Risovany <BagsBoom>
2020-11-10 23:55:30 +0200

    Added third task

---
+++ Lesson 1/Task_3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int need, inone, sobrano, so;
+    cout << "Skolko oreshkov nuzhno belochke - ";
+    cin >> need;
+    cout << "Skolko oreshkov v odnoy shiske - ";
+    cin >> inone;
+    cout << "Skolko shishek bylo sobrano - ";
+    cin >> sobrano;
+    if(inone*sobrano >= need){
+        cout << "YES";
+    }
+        if(inone*sobrano < need){
+        cout << "NO";
+    }
+}
