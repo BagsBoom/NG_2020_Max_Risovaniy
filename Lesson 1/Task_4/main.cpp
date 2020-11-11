c006717079e1b85142d5f4b042df391b17f6a6da
Max Risovany <BagsBoom>
2020-11-11 16:27:05 +0200

    Now it is working with 1k and 1kk

--- Lesson 1/Task_4/main.cpp
+++ Lesson 1/Task_4/main.cpp
@@ -7,7 +7,7 @@ int main()
     int x;
     cout << "Skolko vi zarabativaite? ";
     cin >> x;
-    if (x < 1000){
+    if (x < 1000+1){
         cout << "Rabotai bolshe!";
     }
     if (x > 1000){
@@ -15,7 +15,7 @@ int main()
             cout << "Tak derzhat!";
         }
     }
-    if(x > 1000000){
+    if(x > 1000000-1){
         cout << "Ti millioner!";
     }
     cout << "No ti molodec!";
