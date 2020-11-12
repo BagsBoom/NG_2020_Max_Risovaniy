1ff4d718793ef2a7a43342555a00bd717f98da6c
Max Risovany <BagsBoom>
2020-11-12 20:56:16 +0200

    Added Task_7

---
+++ Lesson 1/Task_7/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cout << "Enter number - ";
+    cin >> x;
+    y = x;
+    while (x <= y)
+        {
+        while (z <= x)
+        {
+            cout << z;
+            if (z != x){
+                cout << ", ";
+            }
+            z++;
+        }
+        x++;
+       1 }
+}
