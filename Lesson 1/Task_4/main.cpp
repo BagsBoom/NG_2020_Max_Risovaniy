ef2fe10a3698e9c63f56533edc9c9f8a2bc1053a
Max Risovany <BagsBoom>
2020-11-12 11:19:59 +0200

    Update code style

--- Lesson 1/Task_4/main.cpp
+++ Lesson 1/Task_4/main.cpp
@@ -7,18 +7,23 @@ int main()
     int x;
     cout << "Skolko vi zarabativaite? ";
     cin >> x;
-    if (x < 1001){
+    if (x < 1001)
+    {
         cout << "Rabotai bolshe!";
     }
 
-    if(x > 1000){
-        if(x < 1000000){
-        cout << "Tak derzhat!";
+    if (x > 1000)
+    {
+        if (x < 1000000)
+        {
+            cout << "Tak derzhat!";
         }
-        if(x > 999999){
-        cout << "Ti millioner!";
+        if (x > 999999)
+        {
+            cout << "Ti millioner!";
         }
     }
 
     cout << "No ti molodec!";
 }
+
