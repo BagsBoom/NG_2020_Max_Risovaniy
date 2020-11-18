95196314be6b97d73917ca70cb7e692f4966f505
Max Risovany <BagsBoom>
2020-11-18 19:21:47 +0200

    Added second Task

--- Lesson_2/Task_2/main.cpp
+++ Lesson_2/Task_2/main.cpp
@@ -8,22 +8,25 @@ int main()
     int money = 0;
     int allMoney = 0;
     int i;
-        for(i = 0; i < 10; i++){
-            card[i] = 0;
+    for (i = 0; i < 10; i++)
+    {
+        card[i] = 0;
     }
-    while(true){
+    while (true)
+    {
         cout << "vvedite nomer karti - ";
         cin >> i;
         cout << "skolko polozhit? ";
         cin >> money;
         card[i - 1] = card[i - 1] + money;
-        for(int j = 0; j < 10; j++){
-             cout << card[j]  << " ";
-  }
+        for (int j = 0; j < 10; j++)
+        {
+            cout << card[j] << " ";
+        }
         cout << endl;
         cout << "v summe na vsex kartochkah - ";
         allMoney = allMoney + money;
         cout << allMoney;
         cout << endl;
-}
+    }
 }
