f56260bb8030478052e096cdf8b90f98c1b6c0a7
Max Risovany <BagsBoom>
2020-12-05 00:53:45 +0200

    Added third task

---
+++ Lesson_3/Task_3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char stroka[25];
+    cout << "Enter stroku: ";
+    cin.getline(stroka, 25);
+    int i = 0;
+    int count;
+    while (stroka[i]!=0) {
+        if(stroka[i] >= 97 && stroka[i] <= 122){
+        count = stroka[i];
+        count = count - 32;
+        stroka[i] = count;
+        }
+        i++;
+    }
+    cout << "Result - " << stroka;
+}
