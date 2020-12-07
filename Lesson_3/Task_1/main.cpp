ddc05a2b93f3abc32e81411e8ec0d5e87f0114d3
Max Risovany <BagsBoom>
2020-12-07 15:47:09 +0200

    Updated Task 1

--- Lesson_3/Task_1/main.cpp
+++ Lesson_3/Task_1/main.cpp
@@ -6,22 +6,16 @@ int main()
 {
     char stroka[20];
     int i = 0;
-    int smallW = 0;
-    int num = 0;
-    int bigW = 0;
+    int word = 0;
     cout << "Enter stroku: ";
     cin.getline(stroka, 20);
     for(i = 0; i < 20; i++){
         if(stroka[i] == '\0') break;
         if(stroka[i] >= 97 && stroka[i] <= 122){
-            smallW = 1;
-        }
-        if(stroka[i] >= 48 && stroka[i] <= 57){
-            num = 1;
-        }
-        if(stroka[i] >= 65 && stroka[i] <= 90){
-            bigW = 1;
+            if(!(stroka[i + 1] >= 97 && stroka[i + 1] <= 122)){
+                word++;
+            }
         }
     }
-    cout << "Result: " << smallW + num + bigW << endl;
+    cout << "Result: " << word << endl;
 }
