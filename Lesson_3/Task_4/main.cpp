974e03482093d3b34d69f7f2b4f25e3cb3c09247
Max Risovany <BagsBoom>
2020-12-18 21:17:54 +0200

    Added Task 4

--- Lesson_3/Task_4/main.cpp
+++ Lesson_3/Task_4/main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main()
 {
     char stroka[100];
-    char s;
     cout << "Enter stroku: ";
     cin.getline(stroka, 100);
     int i = 0;
@@ -34,31 +33,65 @@ int main()
         }
         i++;
     }
+
+    int l = 0;
+    int counter = 0;
+    char counts[100];
+
+    for(i = 0; i <= word; i++)
+    {
+        counts[i] = 0;
+    }
+
+    // Подсчет повторений
+    for(int i = 0; i <= word; i++){
+        for(int j = 0; j <= word; j++){
+            int currentSymbol = stroka[i];
+            if(currentSymbol == stroka[j])
+            {
+                counter++;
+            }
+        }
+        counts[l] = counter;
+        l++;
+        counter = 0;
+    }
     /* Отсортируем массив по убыванию */
     for(int i = 1; i < word; i++)
     {
-        for( r = 0; r < word-i; r++)
+        for( r = 0; r < word-1; r++)
         {
-            if(stroka[r] < stroka[r+1])
+            if(counts[r] < counts[r+1])
             {
                 // Обмен местами
-                int temp = stroka[r];
+                int tmp_int = counts[r];
+                counts[r] = counts[r+1];
+                counts[r+1] = tmp_int;
+
+                char tmp_char = stroka[r];
                 stroka[r] = stroka[r+1];
-                stroka[r+1] = temp;
+                stroka[r+1] = tmp_char;
             }
         }
     }
     c = stroka[0];
-                    cout << "Result: " << endl;
+    cout << "Result: " << endl;
     for(int i = 0; i <= word; i++){
 
         if(c != stroka[i])
         {
-            if(c != ' ')
+            if(c != '\0')
             {
-                cout << c << " - " << n << endl;
-                n = 1;
-                c = stroka[i];
+                if(stroka[i] != ' '){
+                    cout << c << " - " << n << endl;
+                    n = 1;
+                    c = stroka[i];
+                }
+                else
+                {
+                    word = word + 1;
+
+                }
             }
         }
         else
