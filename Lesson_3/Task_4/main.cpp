0d812b95a0391924533bcb339b25e33cd809b445
Max Risovany <BagsBoom>
2021-01-15 00:12:07 +0200

    Updated Task 4

--- Lesson_3/Task_4/main.cpp
+++ Lesson_3/Task_4/main.cpp
@@ -7,20 +7,13 @@ int main()
     char stroka[100];
     cout << "Enter stroku: ";
     cin.getline(stroka, 100);
-    int i = 0;
-    int r = 0;
-    int n = 0;
-    char c;
+    int amount = 0;
+    char symbol;
     int word = 0;
     int count;
+    int i = 0;
     while (stroka[i]!= '\0')
     {
-        if(stroka[i] >= 'a' && stroka[i] <= 'z')
-        {
-            count = stroka[i];
-            count = count - 32;
-            stroka[i] = count;
-        }
         if(stroka[i] >= 'A' && stroka[i] <= 'Z')
         {
             count = stroka[i];
@@ -34,7 +27,7 @@ int main()
         i++;
     }
 
-    int l = 0;
+    int iteration = 0;
     int counter = 0;
     char counts[100];
 
@@ -52,40 +45,41 @@ int main()
                 counter++;
             }
         }
-        counts[l] = counter;
-        l++;
+        counts[iteration] = counter;
+        iteration++;
         counter = 0;
     }
+    int sort = 0;
     // Сортировка
     for(int i = 1; i < word; i++)
     {
-        for( r = 0; r < word-1; r++)
+        for( sort = 0; sort < word-1; sort++)
         {
-            if(counts[r] < counts[r+1])
+            if(counts[sort] < counts[sort+1])
             {
                 // Обмен местами
-                int tmp_int = counts[r];
-                counts[r] = counts[r+1];
-                counts[r+1] = tmp_int;
+                int tmp_int = counts[sort];
+                counts[sort] = counts[sort+1];
+                counts[sort+1] = tmp_int;
 
-                char tmp_char = stroka[r];
-                stroka[r] = stroka[r+1];
-                stroka[r+1] = tmp_char;
+                char tmp_char = stroka[sort];
+                stroka[sort] = stroka[sort+1];
+                stroka[sort+1] = tmp_char;
             }
         }
     }
-    c = stroka[0];
+    symbol = stroka[0];
     cout << "Result: " << endl;
     for(int i = 0; i <= word; i++){
 
-        if(c != stroka[i])
+        if(symbol != stroka[i])
         {
-            if(c != '\0')
+            if(symbol != '\0')
             {
                 if(stroka[i] != ' '){
-                    cout << c << " - " << n << endl;
-                    n = 1;
-                    c = stroka[i];
+                    cout << symbol << " - " << amount << endl;
+                    amount = 1;
+                    symbol = stroka[i];
                 }
                 else
                 {
@@ -96,7 +90,7 @@ int main()
         }
         else
         {
-            n++;
+            amount++;
 
         }
     }
