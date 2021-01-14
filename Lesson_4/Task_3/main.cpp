9dce9c841c9cb01f13ec70ef59c5bbc692a77aef
Max Risovany <BagsBoom>
2021-01-15 00:01:27 +0200

    Updated Task 3

--- Lesson_4/Task_3/main.cpp
+++ Lesson_4/Task_3/main.cpp
@@ -2,15 +2,17 @@
 
 using namespace std;
 
+int word = 0;
+
+
 void enterString(char *stroka)
 {
     cout << "Enter stroku: ";
     cin.getline(stroka, 100);
 }
-void countsOfWords(char *stroka)
+void uslovie (char *stroka)
 {
     int i = 0;
-    int word = 0;
     while(stroka[i] != '\0'){
         if((stroka[i] >= 'a' && stroka[i] <= 'z') || (stroka[i] >= 'A' && stroka[i] <= 'Z')){
             if(!(stroka[i + 1] >= 'a' && stroka[i + 1] <= 'z') && !(stroka[i + 1] >= 'A' && stroka[i + 1] <= 'Z')){
@@ -19,6 +21,14 @@ void countsOfWords(char *stroka)
         }
         i++;
     }
+}
+void countsOfWords(char *stroka)
+{
+        uslovie(stroka);
+}
+
+void conclusion ()
+{
     cout << "Result: " << word << endl;
 }
 int main()
@@ -26,4 +36,5 @@ int main()
     char stroka[100];
     enterString(stroka);
     countsOfWords(stroka);
+    conclusion();
 }
