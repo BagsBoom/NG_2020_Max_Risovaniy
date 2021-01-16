eed4d9deb7c4ba916a57784f85a87237e238ea41
Max Risovany <BagsBoom>
2021-01-16 20:17:35 +0200

    Uptaded Task 3

--- Lesson_4/Task_3/main.cpp
+++ Lesson_4/Task_3/main.cpp
@@ -10,21 +10,21 @@ void enterString(char *stroka)
     cout << "Enter stroku: ";
     cin.getline(stroka, 100);
 }
-void uslovie (char *stroka)
+void uslovie (char *stroka, int i)
 {
-    int i = 0;
-    while(stroka[i] != '\0'){
         if((stroka[i] >= 'a' && stroka[i] <= 'z') || (stroka[i] >= 'A' && stroka[i] <= 'Z')){
             if(!(stroka[i + 1] >= 'a' && stroka[i + 1] <= 'z') && !(stroka[i + 1] >= 'A' && stroka[i + 1] <= 'Z')){
                 word++;
             }
         }
-        i++;
-    }
 }
 void countsOfWords(char *stroka)
 {
-        uslovie(stroka);
+    int i = 0;
+    while(stroka[i] != '\0'){
+        uslovie(stroka,i);
+        i++;
+    }
 }
 
 void conclusion ()
