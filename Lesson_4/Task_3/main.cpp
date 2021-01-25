1d3e54db3801172a47f26d0ba1be57c66d03e2ad
Max Risovany <BagsBoom>
2021-01-25 16:54:50 +0200

    Uptaded Task 3

--- Lesson_4/Task_3/main.cpp
+++ Lesson_4/Task_3/main.cpp
@@ -2,39 +2,41 @@
 
 using namespace std;
 
-int word = 0;
-
 
 void enterString(char *stroka)
 {
     cout << "Enter stroku: ";
     cin.getline(stroka, 100);
 }
-void uslovie (char *stroka, int i)
+
+void uslovie (char *stroka, int i,int &word)
 {
-        if((stroka[i] >= 'a' && stroka[i] <= 'z') || (stroka[i] >= 'A' && stroka[i] <= 'Z')){
-            if(!(stroka[i + 1] >= 'a' && stroka[i + 1] <= 'z') && !(stroka[i + 1] >= 'A' && stroka[i + 1] <= 'Z')){
-                word++;
-            }
+    if((stroka[i] >= 'a' && stroka[i] <= 'z') || (stroka[i] >= 'A' && stroka[i] <= 'Z')){
+        if(!(stroka[i + 1] >= 'a' && stroka[i + 1] <= 'z') && !(stroka[i + 1] >= 'A' && stroka[i + 1] <= 'Z')){
+            word++;
         }
+    }
 }
-void countsOfWords(char *stroka)
+
+void countsOfWords(char *stroka,int &word)
 {
     int i = 0;
     while(stroka[i] != '\0'){
-        uslovie(stroka,i);
+        uslovie(stroka,i,word);
         i++;
     }
 }
 
-void conclusion ()
+void conclusion (int word)
 {
     cout << "Result: " << word << endl;
 }
+
 int main()
 {
+    int word = 0;
     char stroka[100];
     enterString(stroka);
-    countsOfWords(stroka);
-    conclusion();
+    countsOfWords(stroka,word);
+    conclusion(word);
 }
